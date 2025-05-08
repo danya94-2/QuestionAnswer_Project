6c68b312233c1e62c9644673d58a4fc3f0b4110b
dnjswjd222 <bnm673@naver.com>
2025-05-08 12:18:32 +0900

    delete 수정...ver0.2

--- src/project/DeleteController.java
+++ src/project/DeleteController.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class DeleteController implements CommControllerInterface{
 
 	static Scanner sc = new Scanner(System.in);
+	static QA_Board_Service qa_Board_Service = new QA_Board_Service();
 	
 	@Override
 	public void execute() {
@@ -14,7 +15,7 @@ public class DeleteController implements CommControllerInterface{
 	private void f_deleteById() {
 		System.out.print("삭제할 게시글 번호>>");
 		int id = sc.nextInt();
-		int result = QA_Board_Service.deleteById(id);
+		int result = qa_Board_Service.deleteById(id);
 		QA_Board_View.display(result + "건 삭제");
 	}
 }
